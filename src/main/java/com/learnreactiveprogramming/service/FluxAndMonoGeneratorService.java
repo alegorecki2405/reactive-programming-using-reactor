49b3c8be7397235fa51039489e09fdd9dc1d1a40
agorecki <alegorecki@gmail.com>
2024-11-27 14:54:24 +0100

    up to lecture 17

---
+++ src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -0,0 +1,35 @@
+package com.learnreactiveprogramming.service;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of("alex", "ben", "chloe")).log();
+    }
+
+    public Mono<String> namesMono() {
+        return Mono.just("alex").log();
+    }
+
+    public Flux<String> namesFlux_map() {
+        return Flux.fromIterable(List.of("alex", "ben", "chloe"))
+                .map(String::toUpperCase)
+//                .map(s->s.toUpperCase())
+                .log();
+    }
+
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+        fluxAndMonoGeneratorService.namesFlux().subscribe(name -> {
+            System.out.println("name is :" + name);
+        });
+
+        fluxAndMonoGeneratorService.namesMono().subscribe(name ->{
+            System.out.println("mono name is :" + name);
+        });
+    }
+}
--- src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,4 +1,40 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
 public class FluxAndMonoGeneratorServiceTest {
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+    @Test
+    void namesFlux() {
+        var namesFlux = fluxAndMonoGeneratorService.namesFlux();
+
+        StepVerifier.create(namesFlux)
+//                .expectNextCount(3)
+//                .expectNext("alex", "ben", "chloe")]
+                .expectNext("alex")
+                .expectNextCount(2)
+                .verifyComplete();
+    }
+
+    @Test
+    void namesMono() {
+        var namesMono = fluxAndMonoGeneratorService.namesMono();
+
+        StepVerifier.create(namesMono)
+                .expectNext("alex")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_map() {
+        var namesFlux = fluxAndMonoGeneratorService.namesFlux_map();
+
+        StepVerifier
+                .create(namesFlux)
+                .expectNext("ALEX","BEN","CHLOE")
+                .verifyComplete();
+    }
 }
